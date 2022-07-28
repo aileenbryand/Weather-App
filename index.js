1c8dbafebc9b11038fdcbee7044180531e6dc219
Aileen Bryand <aileenale09@gmail.com>
2022-07-27 21:55:37 -0500

    edits on js

--- index.js
+++ index.js
@@ -2,14 +2,24 @@
 
 let weather = {
     "apiKey": "2cb0ee244fa901e33d94f3d995fcae46",
-    fetchWeather: function () {
-        fetch ("https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=2cb0ee244fa901e33d94f3d995fcae46"
-        ).then ((response) => response.json())
+    fetchWeather: function (city) {
+        fetch ("https://api.openweathermap.org/data/2.5/weather?q="
+        + city
+        + "&appid=" 
+        + this.apiKey
+        )
+        
+        .then ((response) => response.json())
         .then ((data) => console.log(data));
+    },
+    displayWeather: function (data) {
+        const {name} = data;
+        const { description} = data.weather[2];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name,description,humidity,speed,temp);
 
     }
-  
-
 }
 
 
